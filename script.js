83513b47cafa9ff2c620eba34043c91747f2609e
MandisaT <mandisa.thomas1@gmail.com>
2016-04-14 17:02:36 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function toggle_visibility(id) {
+       var e = document.getElementById(id);
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else
+          e.style.display = 'block';
+    }
+
+
+
+// function startLightBox(){
+// 	var lbt= document.getElementById("lightbox");
+// 	var lbt2= document.getElementById("lightbox2");
+
+// 	lbt.style.display="block";
+// 	lbt2.style.display="block":
+// }
